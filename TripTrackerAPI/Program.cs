2ca65383ef07cb6cdd96018ce13f523c00b86d16
ErsinAgirman <117252660+ErsinAgirman@users.noreply.github.com>
2024-08-17 01:12:39 +0300

    Ajax for Home Index Added

--- TripTrackerAPI/Program.cs
+++ TripTrackerAPI/Program.cs
@@ -31,6 +31,18 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 	options.SuppressModelStateInvalidFilter = true;
 });
 
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("https://localhost:7195")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -68,11 +80,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+}
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection(); //middleware's
+
+app.UseRouting();
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseCustomException();
 
